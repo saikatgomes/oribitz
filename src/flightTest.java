05b4b77892aab696e13d89443a01d6bcd6709431
saikatgomes <saikatgomes@gmail.com>
2014-10-24 15:09:04 -0500

    Read flight and task as inputs

--- src/flightTest.java
+++ src/flightTest.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
 
 public class flightTest {
 
@@ -5,7 +8,46 @@ public class flightTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		if (args.length != 2) {
+			System.out.println("ERROR: (usage) flightTest "
+					+ "<flights-file> <task-file>");
+			System.exit(-1);
+		}
+
+		HashMap<String, String> airports = new HashMap<String, String>();
+
+		BufferedReader in;
+		try {
+			in = new BufferedReader(new FileReader(args[0]));
+			int lineCount = 0;
+			while (in.ready()) {
+				String aLine = in.readLine();
+				lineCount++;
+				String[] data = aLine.split(",");
+				if (data.length != 3) {
+					System.out.println("Flights file (" + args[0]
+							+ ") should be formatted as "
+							+ "<airport1>,<airport2>,<time> [line:" + lineCount
+							+ "]");
+					System.exit(-1);
+				}
+				String ap1=data[0];
+				String ap2=data[1];
+				String hrs=data[2];
+				if(airports.containsKey(ap1)){
+					airports.put(ap1, airports.get(ap1)+"|"+ap2);
+				}else{
+					airports.put(ap1, ap2);
+				}
+				System.out.println(data[0] + " --> " + data[1] + " = "
+						+ data[2]);
+			}
+			in.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		System.out.println(airports.entrySet());
 
 	}
 
